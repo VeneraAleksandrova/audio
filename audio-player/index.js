131220df0f9659687651e8664bf3fbe76295b8e7
Venera Aleksandrova <venera.n.aleksandrova@gmail.com>
2024-09-19 16:58:25 +0300

    feat: add play and pause the song

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,139 +1,76 @@
-const progress = document.getElementById('progress');
-const song = document.getElementById('song');
-const controlIcon = document.getElementById('controlIcon');
-const playPauseButton = document.querySelector('.play-pause-btn');
-const forwardButton = document.querySelector('.controls button.forward');
-const backwardButton = document.querySelector('.controls button.backward');
-const songName = document.querySelector('.music-player h1');
-const artistName = document.querySelector('.music-player p');
-
+const title = document.querySelector('.music-player h1');
+const name = document.querySelector('.song__name');
+const audio = document.querySelector('#song');
+const playBtn = document.querySelector('.play-pause-btn');
+const controlIcon = document.querySelector('#controlIcon');
 const songs = [
   {
     title: 'Symphony',
     name: 'Clean Bandit ft. Zara Larsson',
-    source:
-      'https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Clean-Bandit-Symphony.mp3',
+    source: './music/Clean-Bandit-Symphony.mp3',
   },
   {
     title: 'Pawn It All',
     name: 'Alicia Keys',
-    source:
-      'https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Pawn-It-All.mp3',
+    source: './music/Pawn-It-All.mp3',
   },
   {
     title: 'Seni Dert Etmeler',
     name: 'Madrigal',
-    source:
-      'https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Madrigal-Seni-Dert-Etmeler.mp3',
+    source: './music/Madrigal-Seni-Dert-Etmeler.mp3',
   },
   {
     title: 'Instant Crush',
     name: 'Daft Punk ft. Julian Casablancas',
-    source:
-      'https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Daft-Punk-Instant-Crush.mp3',
+    source: './music/Daft-Punk-Instant-Crush.mp3',
   },
   {
     title: 'As It Was',
     name: 'Harry Styles',
-    source:
-      'https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Harry-Styles-As-It-Was.mp3',
+    source: './music/Harry-Styles-As-It-Was.mp3',
   },
 
   {
     title: 'Physical',
     name: 'Dua Lipa',
-    source:
-      'https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Dua-Lipa-Physical.mp3',
+    source: './music/Dua-Lipa-Physical.mp3',
   },
   {
     title: 'Delicate',
     name: 'Taylor Swift',
-    source:
-      'https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Taylor-Swift-Delicate.mp3',
+    source: './music/Taylor-Swift-Delicate.mp3',
   },
 ];
 
-let currentSongIndex = 3;
+let indexOfSong = 0;
 
-function updateSongInfo() {
-  songName.textContent = songs[currentSongIndex].title;
-  artistName.textContent = songs[currentSongIndex].name;
-  song.src = songs[currentSongIndex].source;
-
-  song.addEventListener('loadeddata', function () {});
+//Отображение названия текущего трека
+function loadSong(index) {
+  title.textContent = songs[index].title;
+  name.textContent = songs[index].name;
+  audio.src = songs[index].source;
 }
 
-song.addEventListener('timeupdate', function () {
-  if (!song.paused) {
-    progress.value = song.currentTime;
-  }
-});
+loadSong(indexOfSong);
 
-song.addEventListener('loadedmetadata', function () {
-  progress.max = song.duration;
-  progress.value = song.currentTime;
-});
+//Включение и отключение трека трека
+let play = false;
+function playSong() {
+  if (!play) {
+    play = true;
+    audio.play();
+    controlIcon.classList.remove('fa-play');
+    controlIcon.classList.add('fa-pause');
+  } else {
+    play = false;
+    audio.pause();
+    controlIcon.classList.remove('fa-pause');
+    controlIcon.classList.add('fa-play');
+  }
+}
 
 function pauseSong() {
-  song.pause();
-  controlIcon.classList.remove('fa-pause');
-  controlIcon.classList.add('fa-play');
+  audio.pause();
 }
 
-function playSong() {
-  song.play();
-  controlIcon.classList.add('fa-pause');
-  controlIcon.classList.remove('fa-play');
-}
-
-function playPause() {
-  if (song.paused) {
-    playSong();
-  } else {
-    pauseSong();
-  }
-}
-
-playPauseButton.addEventListener('click', playPause);
-
-progress.addEventListener('input', function () {
-  song.currentTime = progress.value;
-});
-
-progress.addEventListener('change', function () {
-  playSong();
-});
-
-forwardButton.addEventListener('click', function () {
-  currentSongIndex = (currentSongIndex + 1) % songs.length;
-  updateSongInfo();
-  playPause();
-});
-
-backwardButton.addEventListener('click', function () {
-  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
-  updateSongInfo();
-  playPause();
-});
-
-updateSongInfo();
-
-var swiper = new Swiper('.swiper', {
-  effect: 'coverflow',
-  centeredSlides: true,
-  initialSlide: 3,
-  slidesPerView: 'auto',
-  allowTouchMove: false,
-  spaceBetween: 40,
-  coverflowEffect: {
-    rotate: 25,
-    stretch: 0,
-    depth: 50,
-    modifier: 1,
-    slideShadows: false,
-  },
-  navigation: {
-    nextEl: '.forward',
-    prevEl: '.backward',
-  },
-});
+playBtn.addEventListener('click', playSong);
