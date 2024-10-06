800bd1dcc281848827a0871cc196c0a7c25e7316
Venera Aleksandrova <venera.n.aleksandrova@gmail.com>
2024-10-06 16:12:39 +0300

    feat: add functions

--- audio-player/index.js
+++ audio-player/index.js
@@ -8,42 +8,50 @@ const forward = document.querySelector(".forward");
 const progress = document.querySelector("#progress");
 const currentTime = document.querySelector(".current-time");
 const duration = document.querySelector(".duration");
+const image = document.querySelector(".slide img");
 const songs = [
   {
     title: "Symphony",
     name: "Clean Bandit ft. Zara Larsson",
     source: "./music/Clean-Bandit-Symphony.mp3",
+    img: "./img/1.jpg",
   },
   {
     title: "Pawn It All",
     name: "Alicia Keys",
     source: "./music/Pawn-It-All.mp3",
+    img: "./img/2.JPG",
   },
   {
     title: "Seni Dert Etmeler",
     name: "Madrigal",
     source: "./music/Madrigal-Seni-Dert-Etmeler.mp3",
+    img: "./img/3.JPG",
   },
   {
     title: "Instant Crush",
     name: "Daft Punk ft. Julian Casablancas",
     source: "./music/Daft-Punk-Instant-Crush.mp3",
+    img: "./img/4.JPG",
   },
   {
     title: "As It Was",
     name: "Harry Styles",
     source: "./music/Harry-Styles-As-It-Was.mp3",
+    img: "./img/5.JPG",
   },
 
   {
     title: "Physical",
     name: "Dua Lipa",
     source: "./music/Dua-Lipa-Physical.mp3",
+    img: "./img/6.JPG",
   },
   {
     title: "Delicate",
     name: "Taylor Swift",
     source: "./music/Taylor-Swift-Delicate.mp3",
+    img: "./img/7.JPG",
   },
 ];
 
@@ -58,12 +66,13 @@ function loadSong(index) {
   audio.load();
   name.textContent = songs[index].name;
   title.textContent = songs[index].title;
+  image.src = songs[index].img;
   updateTimer = setInterval(seekUpdate, 1000);
   audio.addEventListener("ended", nextSong);
 }
 function resetValues() {
   currentTime.textContent = "00:00";
-  duration.textContent = "00:00";
+  //duration.textContent = "00:00";
   progress.value = 0;
 }
 
@@ -86,46 +95,49 @@ function playSong() {
     controlIcon.classList.add("fa-play");
   }
 }
-
+function playNotCurrentSong() {
+  if (isPlaying) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+}
 function nextSong() {
   if (indexOfSong < songs.length - 1) indexOfSong += 1;
   else indexOfSong = 0;
-  isPlaying = false;
   loadSong(indexOfSong);
-  playSong();
+  playNotCurrentSong();
 }
 
 function prevSong() {
   if (indexOfSong > 0) indexOfSong -= 1;
   else indexOfSong = songs.length - 1;
-  isPlaying = false;
   loadSong(indexOfSong);
-  playSong();
+  playNotCurrentSong();
 }
 
 loadSong(indexOfSong);
-
-function seekUpdate() {
+function changeDuration() {
   if (!isNaN(audio.duration)) {
-    // Calculate the time left and the total duration
-    let currentMinutes = Math.floor(audio.currentTime / 60);
-    let currentSeconds = Math.floor(audio.currentTime - currentMinutes * 60);
     let durationMinutes = Math.floor(audio.duration / 60);
     let durationSeconds = Math.floor(audio.duration - durationMinutes * 60);
-
-    // Add a zero to the single digit time values
-    currentSeconds =
-      currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds;
     durationSeconds =
       durationSeconds < 10 ? `0${durationSeconds}` : durationSeconds;
-    currentMinutes =
-      currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes;
     durationMinutes =
       durationMinutes < 10 ? `0${durationMinutes}` : durationMinutes;
-
-    // Display the updated duration
-    currentTime.textContent = currentMinutes + ":" + currentSeconds;
     duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+function seekUpdate() {
+  if (!isNaN(audio.duration)) {
+    let currentMinutes = Math.floor(audio.currentTime / 60);
+    let currentSeconds = Math.floor(audio.currentTime - currentMinutes * 60);
+
+    currentSeconds =
+      currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds;
+    currentMinutes =
+      currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes;
+    currentTime.textContent = currentMinutes + ":" + currentSeconds;
   }
 }
 
@@ -135,6 +147,7 @@ forward.addEventListener("click", () => nextSong());
 audio.addEventListener("loadedmetadata", function () {
   progress.max = audio.duration;
   progress.value = audio.currentTime;
+  changeDuration();
 });
 audio.addEventListener("timeupdate", function () {
   if (!audio.paused) {
